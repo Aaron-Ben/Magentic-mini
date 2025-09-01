f82ac0e752173f31847603b2911eb263d30dd53e
xuenai <3140.97643@qq.com>
2025-09-02 00:25:06 +0800

    add: bing_search

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,3 +1,6 @@
 pub mod chromiumoxide;
 pub mod utils;
 pub mod url_status_manager;
+pub mod search;
+
+pub use search::{BingSearchResults, extract_page_markdown, get_bing_search_results};
---
+++ src/tools/search/mod.rs
@@ -0,0 +1,3 @@
+pub mod bing_search;
+
+pub use bing_search::{BingSearchResults, extract_page_markdown, get_bing_search_results};
