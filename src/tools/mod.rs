621a01e8a5f3158b4f5e5f81581a4ab0a4af54e4
xuenai <3140.97643@qq.com>
2025-09-04 01:00:59 +0800

    fix: rename dir chrome

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,6 +1,6 @@
-pub mod chromiumoxide;
+pub mod chrome;
 pub mod utils;
 pub mod url_status_manager;
-pub mod search;
+// pub mod search;
 
-pub use search::{BingSearchResults, extract_page_markdown, get_bing_search_results};
+// pub use search::{BingSearchResults, extract_page_markdown, get_bing_search_results};
